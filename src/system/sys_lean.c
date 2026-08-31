89789edc8800ab811c77ea4e1110d849fd0081a0
camphor-h <pilllllz@outlook.com>
2026-09-01 00:50:43 +0800

    I hope it will be the final fix for send file

--- src/system/sys_lean.c
+++ src/system/sys_lean.c
@@ -37,8 +37,12 @@
 #ifdef __linux__
 #include <sys/sendfile.h>
 #ifndef __GLIBC__
-    #define copy_file_range(fd_in, off_in, fd_out, off_out, len, flags) \
-        syscall(__NR_copy_file_range, fd_in, off_in, fd_out, off_out, len, flags)
+#include <sys/syscall.h>
+ssize_t copy_file_range(int fd_in, off_t *off_in, int fd_out, 
+                        off_t *off_out, size_t len, unsigned int flags)
+{
+    return syscall(SYS_copy_file_range, fd_in, off_in, fd_out, off_out, len, flags);
+}
 #endif
 #endif
 
